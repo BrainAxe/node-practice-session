a6d063b9a2f210e10096691dd4adea4c1c142972
BrainAxe <tanzimrizwan@gmail.com>
2021-01-20 17:02:57 +0600

    fix remove product from cart

--- first-app_v2/controllers/shop.js
+++ first-app_v2/controllers/shop.js
@@ -62,10 +62,10 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	const prodPrice = req.body.productPrice;
-	
-	Cart.deleteProduct(prodId, prodPrice);
-	res.redirect('/cart');
+	Product.findById(prodId, product => {
+		Cart.deleteProduct(prodId, product.price);
+		res.redirect('/cart');
+	  });
 };
 
 exports.getOrders = (req, res, next) => {
