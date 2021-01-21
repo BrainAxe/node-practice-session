8c168c56c63991dca5c5ceca10e1a6bbf35dc4f8
BrainAxe <tanzimrizwan@gmail.com>
2021-01-21 12:03:32 +0600

    add cart and cartItem model

--- first-app_v3/controllers/admin.js
+++ first-app_v3/controllers/admin.js
@@ -33,8 +33,10 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect('/');
 	}
 	const prodId = req.params.productId;
-	Product.findByPk(prodId)
-	.then(product => {
+	req.user.getProducts({where: {id: prodId}})
+	// Product.findByPk(prodId)
+	.then(products => {
+		const product = products[0];
 		if (!product) {
 			return res.redirect('/');
 		}
@@ -70,7 +72,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.findAll().then(products => {
+	// Product.findAll()
+	req.user.getProducts()
+	.then(products => {
 		res.render('admin/products', {
 			prods: products,
 			pageTitle: 'Admin Products',
