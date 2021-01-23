ed710726e47ad6bed2bf966c105e5ccea1fa2714
BrainAxe <tanzimrizwan@gmail.com>
2021-01-23 13:20:43 +0600

    add product add in mongo

--- first-app_v4/app.js
+++ first-app_v4/app.js
@@ -4,15 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,18 +21,17 @@ app.use((req, res, next) => {
 	// User.findByPk(1)
 	// .then(user => {
 	// 	req.user = user;
-	// 	next();
+		next();
 	// })
 	// .catch(err => console.log(err));
 })
 
 
-// app.use('/admin', adminRoutes);
-// app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-	console.log(client);
+mongoConnect(() => {
 	app.listen(3000);
 })
--- first-app_v4/util/database.js
+++ first-app_v4/util/database.js
@@ -1,13 +1,27 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db; // UnderScore is just a signal that this will only use in this file
+
 const mongoConnect = (callback) => {
-	MongoClient.connect('mongodb+srv://tanzim:9ggZnKuD6BY@cluster0.99q9l.mongodb.net/<dbname>?retryWrites=true&w=majority')
+	MongoClient.connect('mongodb+srv://tanzim:9ggZnKuD6BY@cluster0.99q9l.mongodb.net/shop?retryWrites=true&w=majority')
 	.then(client => {
 		console.log("Connected!");
-		callback(client);
+		_db = client.db();
+		callback();
 	})
-	.catch(err => console.log(err));
+	.catch(err => {
+		console.log(err);
+		throw err;
+	});
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+	if (_db) {
+		return _db;
+	}
+	throw 'No database found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
