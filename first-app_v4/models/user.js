91d75223388204005eb9af057dc731585af67464
BrainAxe <tanzimrizwan@gmail.com>
2021-01-26 23:39:13 +0600

    multiple product insert into cart functionality

--- first-app_v4/models/user.js
+++ first-app_v4/models/user.js
@@ -1,43 +1,65 @@
-const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
-
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
 class User {
-	constructor(username, email, cart, id) {
-		this.name = username;
-		this.email = email;
-		this.cart = cart;
-		this._id = id;
-	}
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
+  }
 
-	save() {
-		const db = getDb();
-		return db.collection('users').insertOne(this)
-		.then(result => {
-			console.log(result);
-		})
-		.catch(err => console.log(err));
-	}
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => console.log(err));
+  }
 
-	addToCart(product) {
-		// const cartProduct = this.cart.items.findIndex(cp => {
-		// 	return cp._id === product._id;
-		// });
-		const updatedCart = { items: [{productId: new mongodb.ObjectId(product._id), quantity: 1}]};
-		const db = getDb();
-		return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: {cart: updatedCart}});
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-	}
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
 
-	static findById(userId) {
-		const db = getDb();
-		return db.collection('users').find({ _id: new mongodb.ObjectId(userId) }).next()
-		.then(user => {
-			console.log(user);
-			return user;
-		})
-		.catch(err => console.log(err));		
-	}
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .find({ _id: new mongodb.ObjectId(userId) })
+      .next()
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => console.log(err));
+  }
 }
 
 module.exports = User;
