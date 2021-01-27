1b56b1281523e0b288ce35be0ec976df01edaf9b
BrainAxe <tanzimrizwan@gmail.com>
2021-01-27 23:04:00 +0600

    add delete cart item functionality

--- first-app_v4/controllers/shop.js
+++ first-app_v4/controllers/shop.js
@@ -56,46 +56,12 @@ exports.postCart = (req, res, next) => {
       console.log(result);
       res.redirect("/cart");
     });
-  // let fetchedCart;
-  // let newQnty = 1;
-  // req.user.getCart()
-  // .then(cart => {
-  // 	fetchedCart = cart;
-  // 	return cart.getProducts({where: {id: prodId}})
-  // })
-  // .then(products => {
-  // 	let product;
-  // 	if (products.length > 0) {
-  // 		product = products[0];
-  // 	}
-  // 	if (product) {
-  // 		const oldQnty = product.cartItem.quantity;
-  // 		newQnty = oldQnty + 1;
-  // 		return product;
-  // 	}
-  // 	return Product.findByPk(prodId);
-  // })
-  // .then(product => {
-  // 	return fetchedCart.addProduct(product, {
-  // 		through: { quantity: newQnty }
-  // 	});
-  // })
-  // .then(() => {
-  // 	res.redirect('/cart');
-  // })
-  // .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
 	const prodId = req.body.productId;
-	req.user.getCart()
-	.then(cart => {
-		return cart.getProducts({ where: {id: prodId }});
-	})
-	.then(products => {
-		const product = products[0];
-		product.cartItem.destroy();
-	})
+	req.user
+	.deleteItemFromCart(prodId)
 	.then(result =>{
 		res.redirect('/cart');
 	})
