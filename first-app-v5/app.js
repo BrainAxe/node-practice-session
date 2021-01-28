e89a59078dcfe05f174346e79c320a29cd423614
BrainAxe <tanzimrizwan@gmail.com>
2021-01-28 16:20:28 +0600

    add mongoose

---
+++ first-app-v5/app.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById("600ee350a90422b22b569541")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose.connect(process.env.DB_CONN)
+.then(result => {
+	app.listen(3000);
+})
+.catch(err => {
+	console.log(err);
+})
