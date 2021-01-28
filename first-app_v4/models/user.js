8152e97889cc706a9af71ed9a3ed3aae6e916a24
BrainAxe <tanzimrizwan@gmail.com>
2021-01-28 15:26:21 +0600

    mod: modified insert order

--- first-app_v4/models/user.js
+++ first-app_v4/models/user.js
@@ -84,9 +84,17 @@ class User {
 
   addOrder() {
     const db = getDb();
-    return db
-      .collection("orders")
-      .insertOne(this.cart)
+    return this.getCart()
+      .then((products) => {
+        const order = {
+          items: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+          },
+        };
+        return db.collection("orders").insertOne(order);
+      })
       .then((result) => {
         this.cart = { items: [] };
         return db
@@ -96,6 +104,11 @@ class User {
             { $set: { cart: { items: [] } } }
           );
       });
+  }
+
+  getOrders() {
+    const db = getDb();
+    //  return db.collection("orders")
   }
 
   static findById(userId) {
