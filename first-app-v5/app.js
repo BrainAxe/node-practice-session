069fefd7a30df6671942935b32fd308310a6e560
BrainAxe <tanzimrizwan@gmail.com>
2021-01-30 23:23:42 +0600

    add user model

--- first-app-v5/app.js
+++ first-app-v5/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById("600ee350a90422b22b569541")
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("6015950c11329140086e10bc")
+    .then((user) => {
+      req.user =  user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 
 app.use('/admin', adminRoutes);
@@ -34,10 +34,23 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(process.env.DB_CONN)
-.then(result => {
-	app.listen(3000);
-})
-.catch(err => {
-	console.log(err);
-})
+mongoose
+  .connect(process.env.DB_CONN)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Tanzim",
+          email: "tanzim@mail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
