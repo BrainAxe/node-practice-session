a37700e1de2027ee4ee46ad4c50b739a14fae405
BrainAxe <tanzimrizwan@gmail.com>
2021-02-05 22:23:51 +0600

    add session store

--- first-app-v5/app.js
+++ first-app-v5/app.js
@@ -5,11 +5,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+	uri: process.env.DB_CONN,
+	collection: 'session'
+});
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +26,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: "long string value", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: "long string value",
+    resave: false,
+    saveUninitialized: false,
+	store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("6015950c11329140086e10bc")
