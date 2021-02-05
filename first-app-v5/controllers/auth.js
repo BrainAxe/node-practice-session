d66aad8c334fdfafa2f557cbd1a1afacf93f32f5
BrainAxe <tanzimrizwan@gmail.com>
2021-02-05 23:38:02 +0600

    add logout functionality

--- first-app-v5/controllers/auth.js
+++ first-app-v5/controllers/auth.js
@@ -1,19 +1,28 @@
-exports.getLogin = (req, res, next) => {
-//   const isLoggedIn = req
-//   .get("Cookie")
-//   .split(";")[2]
-//   .trim()
-//   .split("=")[1]==='true';
+const User = require('../models/user');
 
+
+exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
   });
 };
-
 exports.postLogin = (req, res, next) => {
-	// res.setHeader('Set-Cookie', 'loggedIn=true');
-	req.session.isLoggedIn = true;
-	res.redirect('/');
-  };
+  User
+    .findById("6015950c11329140086e10bc")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogOut = (req, res, next) => {
+  // res.setHeader('Set-Cookie', 'loggedIn=true');
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
