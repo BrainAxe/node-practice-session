77bad8a77e8197512925abc9241bce7aa09dcc7b
BrainAxe <tanzimrizwan@gmail.com>
2021-02-06 17:22:05 +0600

    add login email and password matching

--- first-app-v5/controllers/auth.js
+++ first-app-v5/controllers/auth.js
@@ -20,13 +20,23 @@ exports.getSignup = (req, res, next) => {
   };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("6015950c11329140086e10bc")
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
     .then((user) => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save((err) => {
-        console.log(err);
-        res.redirect("/");
+      if (!user) {
+        return res.redirect("/login");
+      }
+      bcrypt.compare(password, user.password).then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            res.redirect("/");
+          });
+        }
+        res.redirect("/login");
       });
     })
     .catch((err) => console.log(err));
