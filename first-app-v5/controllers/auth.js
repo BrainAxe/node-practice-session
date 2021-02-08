a51360fb26f21b2ad785f028542c1f8c93925cbd
BrainAxe <tanzimrizwan@gmail.com>
2021-02-08 21:22:37 +0600

    add reset password page

--- first-app-v5/controllers/auth.js
+++ first-app-v5/controllers/auth.js
@@ -115,3 +115,17 @@ exports.postSignup = (req, res, next) => {
       console.log(err);
     });
 };
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
