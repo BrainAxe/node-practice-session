5e1022a05e7f88363b25130a0fde568c1bd4f3f7
BrainAxe <tanzimrizwan@gmail.com>
2021-02-10 23:44:31 +0600

    add more validators

--- first-app-v5/controllers/auth.js
+++ first-app-v5/controllers/auth.js
@@ -78,51 +78,37 @@ exports.postLogout = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const name = email.split("@")[0];
   const errors = validationResult(req);
+  console.log(errors.array());
 
   if (!errors.isEmpty()) {
-    return res.status(422)
-	.render("auth/signup", {
+    return res.status(422).render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
     });
   }
 
-  User.findOne({ email: email })
-    .then((userDoc) => {
-      if (userDoc) {
-        req.flash(
-          "error",
-          "E-Mail exists already, please pick a different one."
-        );
-        return res.redirect("/signup");
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then((hasedPassword) => {
-          const user = new User({
-            name: name,
-            email: email,
-            password: hasedPassword,
-            cart: { item: [] },
-          });
-          return user.save();
-        })
-        .then((result) => {
-          res.redirect("/login");
-          return transporter.sendMail({
-            to: email,
-            from: process.env.MAIL_USER,
-            subject: "Signup Succeeded!",
-            html: "<h1>You successfully signed up!</h1>",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  bcrypt
+    .hash(password, 12)
+    .then((hasedPassword) => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hasedPassword,
+        cart: { item: [] },
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      return transporter.sendMail({
+        to: email,
+        from: process.env.MAIL_USER,
+        subject: "Signup Succeeded!",
+        html: "<h1>You successfully signed up!</h1>",
+      });
     })
     .catch((err) => {
       console.log(err);
--- first-app-v5/routes/auth.js
+++ first-app-v5/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
+
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -13,7 +15,31 @@ router.get('/signup', authController.getSignup);
 
 router.post(
   "/signup",
-  check("email").isEmail().withMessage("Please enter a valid E-mail"),
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid E-mail")
+      .custom((value, { req }) => {
+		return User.findOne({ email: value })
+		.then((userDoc) => {
+		  if (userDoc) {
+			return Promise.reject('E-Mail exists already, please pick a different one.');
+		  }
+		});
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match");
+      }
+      return true;
+    }),
+  ],
   authController.postSignup
 );
 
