843c6df4fdb50e25c4b02a7dbdcc3fa85cc8ee76
BrainAxe <tanzimrizwan@gmail.com>
2021-02-17 23:35:10 +0600

    add download invoice filename

--- first-app-v5/controllers/shop.js
+++ first-app-v5/controllers/shop.js
@@ -148,6 +148,8 @@ exports.getInvoice = (req, res, next) => {
 		if (err) {
 			return next(err);
 		}
+		res.setHeader('Content-Type', 'application/pdf');
+		res.setHeader('Content-Disposition', 'attachment; filename="' + invoiceName + '"')
 		res.send(data);
 	});
 }
