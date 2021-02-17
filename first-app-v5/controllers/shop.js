db1fea9cc5c1658b1aa386f28ecb4ad6833107e1
BrainAxe <tanzimrizwan@gmail.com>
2021-02-18 00:03:01 +0600

    modify invoice download

--- first-app-v5/controllers/shop.js
+++ first-app-v5/controllers/shop.js
@@ -153,18 +153,26 @@ exports.getInvoice = (req, res, next) => {
       const orderId = req.params.orderId;
       const invoiceName = "invoice-" + orderId + ".pdf";
       const invoicePath = path.join("data", "invoices", invoiceName);
-      fs.readFile(invoicePath, (err, data) => {
-        if (err) {
-          return next(err);
-        }
-        res.setHeader("Content-Type", "application/pdf");
-        res.setHeader(
-          "Content-Disposition",
+      //   fs.readFile(invoicePath, (err, data) => {
+      //     if (err) {
+      //       return next(err);
+      //     }
+      //     res.setHeader("Content-Type", "application/pdf");
+      //     res.setHeader(
+      //       "Content-Disposition",
+      //     //   'attachment; filename="' + invoiceName + '"'
+      //       'inline; filename="' + invoiceName + '"'
+      //     );
+      //     res.send(data);
+      //   });
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
         //   'attachment; filename="' + invoiceName + '"'
-          'inline; filename="' + invoiceName + '"'
-        );
-        res.send(data);
-      });
+        'inline; filename="' + invoiceName + '"'
+      );
+      file.pipe(res);
     })
     .catch((err) => next(err));
 };
