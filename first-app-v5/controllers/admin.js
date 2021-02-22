31ee815721d4bcc23cf3ae13109e56b018b54f3f
BrainAxe <tanzimrizwan@gmail.com>
2021-02-23 00:35:08 +0600

    add async-requests in delete product

--- first-app-v5/controllers/admin.js
+++ first-app-v5/controllers/admin.js
@@ -178,23 +178,21 @@ exports.getProducts = (req, res, next) => {
 	  });
 };
 
-exports.postDeleteProducts = (req, res, next) => {
-	const prodId = req.body.productId;
-	Product.findById(prodId)
-	.then(product => {
-		if (!product) {
-			return next(new Error('Product not found.'));
-		}
-		fileHelper.deleteFile(product.imageUrl);
-		return Product.deleteOne({ _id: prodId, userId: req.user._id });
-	})
-	.then(result => {
-		console.log("Destroyed Product");
-		res.redirect('/admin/products');
-	})
-	.catch((err) => {
-		const error = new Error(err);
-		error.httpStatusCode = 500;
-		return next(error);
-	  });
-}
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then((product) => {
+      if (!product) {
+        return next(new Error("Product not found."));
+      }
+      fileHelper.deleteFile(product.imageUrl);
+      return Product.deleteOne({ _id: prodId, userId: req.user._id });
+    })
+    .then((result) => {
+      console.log("Destroyed Product");
+      res.status(200).json({ message: "Success!" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Deleting product failed." });
+    });
+};
