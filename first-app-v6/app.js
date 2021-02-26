33a3a1c5b1eff68ca1c25a426411fa193e6b0b27
BrainAxe <tanzimrizwan@gmail.com>
2021-02-26 23:46:29 +0600

    fix cors error

--- first-app-v6/app.js
+++ first-app-v6/app.js
@@ -9,11 +9,10 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 })
-
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
