f98e616cbbcdd4b8b07cb89a187dd60555eb40a4
BrainAxe <tanzimrizwan@gmail.com>
2021-02-28 23:13:20 +0600

    add error handler and static image serve folder

--- first-app-v6/app.js
+++ first-app-v6/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,6 +10,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,6 +19,13 @@ app.use((req, res, next) => {
   next();
 })
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message} );
+})
 
 mongoose
   .connect(process.env.DB_CONN)
