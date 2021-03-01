9e8043fdd99eaf03bc5ea5b987da2b927d345c26
BrainAxe <tanzimrizwan@gmail.com>
2021-03-02 00:31:06 +0600

    add post delete api

--- first-app-v6/controllers/feed.js
+++ first-app-v6/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const { findByIdAndRemove } = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
   Post.find()
@@ -114,6 +115,31 @@ exports.updatePost = (req, res, next) => {
     })
     .then((result) => {
       res.status(200).json({ message: "Post updated!", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error("Could not find post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      // check logged in User
+      clearImage(post.imageUrl);
+      return Post.findByIdAndRemove(postId);
+
+    }).then(result => {
+      console.log(result);
+      res.status(200).json({ message: 'Deleted post.'});
     })
     .catch((err) => {
       if (!err.statusCode) {
