3e6fe98a8ae79a68517aaebfff4b907956fd9e56
BrainAxe <tanzimrizwan@gmail.com>
2021-03-02 23:40:49 +0600

    add user model

---
+++ first-app-v6/routes/auth.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+// const userController = require('../controllers/feed');
+
+const router = express.Router();
+
+router.put('/signup');
+
+// router.get('/posts', feedController.getPosts);
+
+// router.post(
+//   "/post",
+//   [
+//     body("title").trim().isLength({ min: 5 }),
+//     body("content").trim().isLength({ min: 5 }),
+//   ],
+//   feedController.createPost
+// );
+
+// router.get('/post/:postId', feedController.getPost);
+
+// router.put(
+//   "/post/:postId",
+//   [
+//     body("title").trim().isLength({ min: 5 }),
+//     body("content").trim().isLength({ min: 5 }),
+//   ],
+//   feedController.updatePost
+// );
+
+// router.delete("/post/:postId", feedController.deletePost);
+
+module.exports = router;
