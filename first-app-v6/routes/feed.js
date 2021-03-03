7b028e93e8099639afa55f76ea2b96a2a28fcdeb
BrainAxe <tanzimrizwan@gmail.com>
2021-03-04 00:16:31 +0600

    add check auth middleware

--- first-app-v6/routes/feed.js
+++ first-app-v6/routes/feed.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 
 router.post(
   "/post",
+  isAuth, 
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -16,10 +18,11 @@ router.post(
   feedController.createPost
 );
 
-router.get('/post/:postId', feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 router.put(
   "/post/:postId",
+  isAuth, 
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -27,6 +30,6 @@ router.put(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
