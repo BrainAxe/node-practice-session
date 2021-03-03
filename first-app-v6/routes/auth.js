68f6a8a03526392825114960eea659f664affa9d
BrainAxe <tanzimrizwan@gmail.com>
2021-03-03 23:49:48 +0600

    add login api

--- first-app-v6/routes/auth.js
+++ first-app-v6/routes/auth.js
@@ -27,28 +27,7 @@ router.put(
   authController.signup
 );
 
-// router.get('/posts', feedController.getPosts);
 
-// router.post(
-//   "/post",
-//   [
-//     body("title").trim().isLength({ min: 5 }),
-//     body("content").trim().isLength({ min: 5 }),
-//   ],
-//   feedController.createPost
-// );
-
-// router.get('/post/:postId', feedController.getPost);
-
-// router.put(
-//   "/post/:postId",
-//   [
-//     body("title").trim().isLength({ min: 5 }),
-//     body("content").trim().isLength({ min: 5 }),
-//   ],
-//   feedController.updatePost
-// );
-
-// router.delete("/post/:postId", feedController.deletePost);
+router.post("/login", authController.login);
 
 module.exports = router;
