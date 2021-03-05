ec9b4c5f67a4124123cc169f5386f2a27513e25c
BrainAxe <tanzimrizwan@gmail.com>
2021-03-05 23:35:55 +0600

    initial setup for socket.io

--- first-app-v6/app.js
+++ first-app-v6/app.js
@@ -56,7 +56,12 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.DB_CONN)
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected!');
+    })
+
   })
   .catch((err) => {
     console.log(err);
