eb7454c09716074a593139dedcface0631d772ee
BrainAxe <tanzimrizwan@gmail.com>
2021-03-06 23:50:01 +0600

    add user status api

--- first-app-v6/routes/auth.js
+++ first-app-v6/routes/auth.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const User = require('../models/user');
-
 const authController = require('../controllers/auth');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
@@ -29,5 +29,14 @@ router.put(
 
 
 router.post("/login", authController.login);
+
+router.get("/status", isAuth, authController.getUserStatus);
+
+router.patch(
+  "/status",
+  isAuth,
+  [body("status").trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
